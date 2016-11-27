6496bbfbe934d204be6cb40ec14d4e494567903b
Jeanlouis Rebello <jeanlouisrebello@gmail.com>
2016-11-27 16:31:31 -0500

    Initial Commit

---
+++ Wake Forsure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wake Forsure
+//
+//  Created by Jeanlouis Rebello on 2016-11-27.
+//  Copyright © 2016 DistinctApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
